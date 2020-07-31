38cfecf352cffc21c2d7f0c01c4eb4c05cf16e0b
Arif Hidayat <sharerifdotcom@gmail.com>
2020-07-31 17:49:27 +0700

    Release 1

--- Animal.php
+++ Animal.php
@@ -2,11 +2,27 @@
 
 class Animal{
     public $name;
-    public $legs = 2;
+    public $legs;
     public $cold_blooded = false;
 
     public function __construct($name){
         $this->name = $name;
+    }
+
+    public function set_legs($legs){
+        $this->legs = $legs;
+    }
+
+    public function get_legs(){
+        return $this->legs;
+    }
+
+    public function set_cold_blooded($cold_blooded){
+        $this->cold_blooded = $cold_blooded;
+    }
+
+    public function get_cold_blooded(){
+        return $this->cold_blooded;
     }
 }
 
--- index.php
+++ index.php
@@ -1,26 +1,33 @@
 <?php
 
+// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
 require "Animal.php";
 require "Frog.php";
 require "Ape.php";
 
+
 $sheep = new Animal("shaun");
+$sheep->set_legs(2);
+$sheep->set_cold_blooded(false);
+echo "Name : " .$sheep->name. "<br>"; // "shaun"
+echo "Legs : " .$sheep->get_legs(). "<br>"; // 2
+echo "Cold Blooded : " .$sheep->get_cold_blooded(). "<br><br>"; // false
 
-echo $sheep->name. "<br>"; // "shaun"
-echo $sheep->legs. "<br>"; // 2
-echo $sheep->cold_blooded. "<br><br>"; // false
-
-// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
-
-// index.php
 $sungokong = new Ape("kera sakti");
-echo $sungokong->name. "<br>";
+$sungokong->set_legs(2);
+$sungokong->set_cold_blooded(false);
+echo "Name : " .$sungokong->name. "<br>";
+echo "Legs : " .$sungokong->get_legs(). "<br>";
 $sungokong->yell(); // "Auooo"
-
-echo "<br><br>";
+echo "<br>Cold Blooded : " .$sungokong->get_cold_blooded(). "<br><br>";
 
 $kodok = new Frog("buduk");
-echo $kodok->name. "<br>";
+$kodok->set_legs(4);
+$kodok->set_cold_blooded(true);
+echo "Name : " .$kodok->name. "<br>";
+echo "Legs : " .$kodok->get_legs(). "<br>";
 $kodok->jump() ; // "hop hop"
+echo "<br>Cold Blooded : " .$kodok->get_cold_blooded(). "<br><br>";
 
 ?>
